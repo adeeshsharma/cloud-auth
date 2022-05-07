dccb9e565e02e76e5f92a979dd8ebfc8d00e2917
adeeshsharma <adeeshon@gmail.com>
2022-05-07 16:09:24 +0530

    first build

---
+++ client/build/precache-manifest.ca395978ff65306acd0029aaafb916f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57e25f83719566eb8892c52fec34290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b600cbdc69d1860a810",
+    "url": "/static/css/main.0cbd93e2.chunk.css"
+  },
+  {
+    "revision": "3ef467a0894fdbc3104e",
+    "url": "/static/js/2.9cfcc8b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9cfcc8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b600cbdc69d1860a810",
+    "url": "/static/js/main.b7697543.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a2f60201857b336e7c31193fe8d2d3d8",
+    "url": "/static/media/showcase.a2f60201.jpg"
+  }
+]);
